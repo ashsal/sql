1fb96d1055ce4c62ee28dee110f9eb5f3a8c6c77
Ashish Saluja <ashishsaluja1@gmail.com>
2023-11-06 13:58:25 +0100

    Schema Database

---
+++ SchemaDatabase.sql
@@ -0,0 +1,30 @@
+create schema sell_clothes;
+
+show databases;
+
+use sell_clothes;
+
+create table users(
+	first_name varchar(100),
+    last_name varchar(100)
+);
+
+create table products(
+	name varchar(100),
+    price int
+);
+
+create database sell_shoes; 
+
+use sell_shoes;
+
+
+create table users(
+	first_name varchar(100),
+    last_name varchar(100)
+);
+
+create table products(
+	name varchar(100),
+    price int
+);
