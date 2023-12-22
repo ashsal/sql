a3857b010aa35c63eba6b5af5981ee90ff4fa615
Ashish Saluja <ashishsaluja1@gmail.com>
2023-12-22 14:34:33 +0100

    Select comparison operator

---
+++ SelectComparisonOperator.sql
@@ -0,0 +1,44 @@
+CREATE TABLE products_5 (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(50),
+    stock_quantity INT,
+    manufacturer VARCHAR(50)
+);
+
+INSERT INTO products_5 (product_id, product_name, price, category, stock_quantity, manufacturer)
+VALUES
+    (1, 'Laptop', 999.99, 'Electronics', 20, 'Dell'),
+    (2, 'Smartphone', 599.99, 'Electronics', 30, 'Samsung'),
+    (3, 'Tablet', 299.99, 'Electronics', 15, 'Apple'),
+    (4, 'Smartwatch', 149.99, 'Wearable', 25, 'Fitbit'),
+    (5, 'Headphones', 79.99, 'Audio', 50, 'Sony'),
+    (6, 'Printer', 199.99, 'Office', 15, 'HP'),
+    (7, 'Camera', 349.99, 'Electronics', 10, 'Canon'),
+    (8, 'Desk Chair', 79.99, 'Furniture', 20, 'IKEA'),
+    (9, 'External Hard Drive', 129.99, 'Electronics', 25, 'Seagate'),
+    (10, 'Running Shoes', 59.99, 'Apparel', 30, 'Nike');
+
+select * from products_5;
+select * from products_5 where price = 79.99;
+
+select * from products_5 where manufacturer = 'Apple';
+
+select * from products_5 where manufacturer = 'applE';
+
+select * from products_5 where price <> 79.99;
+select * from products_5 where price != 79.99;
+
+select * from products_5 where manufacturer <> 'applE';
+
+select * from products_5 where price > 349.99;
+
+select product_id, product_name from products_5 where price > 349.99;
+
+select * from products_5 where price < 349.99;
+
+
+select * from products_5 where price >= 349.99;
+
+select * from products_5 where price <= 349.99;
