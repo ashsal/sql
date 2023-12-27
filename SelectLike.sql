a3c8047678c9d69c971efdc28dbb9d80cd3b2c2f
Ashish Saluja <ashishsaluja1@gmail.com>
2023-12-27 14:17:52 +0100

    Select like query

---
+++ SelectLike.sql
@@ -0,0 +1,22 @@
+select * from products_5;
+
+select * from products_5 where manufacturer like 'A%';
+
+
+select * from products_5 where manufacturer like 'a%';
+
+select * from products_5 where manufacturer like 'ap%';
+select * from products_5 where manufacturer like 'apple%';
+
+select * from products_5 where manufacturer like 's%';
+select * from products_5 where manufacturer like 'sa%';
+
+select * from products_5 where manufacturer like '%ung';
+
+select * from products_5 where manufacturer like '%ms%';
+
+select * from products_5 where manufacturer like '_p%';
+select * from products_5 where manufacturer like '_pp%';
+
+select * from products_5 where manufacturer like '_p';
+select * from products_5 where manufacturer like 'h_';
