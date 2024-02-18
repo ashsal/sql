4e00856e2654eedb9ecbf20a38cd46f27129789e
Ashish Saluja <ashishsaluja1@gmail.com>
2024-02-18 13:27:54 +0400

    Full text natural language mode

---
+++ FullTextNaturalLanguage.sql
@@ -0,0 +1,39 @@
+CREATE TABLE articles_full (
+    article_id INT PRIMARY KEY,
+    content TEXT
+);
+
+INSERT INTO articles_full (article_id, content) VALUES
+    (1, 'MySQL is a popular open-source relational database management system (RDBMS).'),
+    (2, 'Full-text search in MySQL enables efficient searching of text data in tables.'),
+    (3, 'Natural language search allows users to express search queries in everyday language.'),
+    (4, 'Indexing improves query performance by enabling faster data retrieval from tables.'),
+    (5, 'performance by enabling');
+    
+CREATE FULLTEXT INDEX articles_full_index_content ON articles_full(content);
+
+select * from articles_full;
+
+
+select * from articles_full where match(content) against('database management' in natural language mode);
+
+
+select * from articles_full where match(content) against('database management');
+
+
+select * from articles_full where match(content) against('performance enabling' in natural language mode);
+
+
+select * from articles_full where match(content) against('performance enabling');
+
+
+
+select * from articles_full where match(content) against('performance enabling');
+
+select * from articles_full where content = 'performance enabling'
+
+
+select * from articles_full where content like '%performance enabling%';
+
+
+select * from articles_full where content like '%performance%' and content like '%enabling%'
