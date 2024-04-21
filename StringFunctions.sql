327333fac2311ef7b3da5ae931882a606205f195
Ashish Saluja <ashishsaluja1@gmail.com>
2024-04-21 14:32:40 +0400

    String functions

---
+++ StringFunctions.sql
@@ -0,0 +1,38 @@
+select concat('Ashish', ' ', 'Saluja');
+
+select concat(firstName, ' ', lastName) from users;
+
+select length('ashish');
+
+select  LOWER('ASHISH1');
+
+select upper('ashish')
+
+select substring('hello world', 7);
+
+select left('hello world', 5);
+
+select right('hello world', 5);
+
+select replace('hello world', 'world', 'universe');
+select replace('hello world', 'World', 'universe');
+
+select trim('  ashish ');
+
+select ltrim('  ashish ');
+select rtrim('  ashish ');
+
+select locate('l', 'hello');
+select instr('hello', 'l');
+
+select strcmp('ashish', 'ashish');
+select strcmp('ashish', 'ashish1');
+
+select strcmp('a', 'b');
+select strcmp('b', 'a');
+
+select strcmp('ashish', 'ASHISH');
+
+
+
+
